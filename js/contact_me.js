4ccf52b83900b06715b0f05d637c37264c15ba8e
Trevor Schachner <51335730+trevorschachner@users.noreply.github.com>
2021-11-08 00:10:36 -0500

    fix to form and js file causing submission problem

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,4 +1,4 @@
-$(function() {
+/* $(function() {
 
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -64,7 +64,7 @@ $(function() {
 });
 
 
-/*When clicking on Full hide fail/success boxes */
+// When clicking on Full hide fail/success boxes //
 $('#name').focus(function() {
     $('#success').html('');
 });
